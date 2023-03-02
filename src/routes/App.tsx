9be37331d9aa1df971dd9444f1448fc42bfb4998
Marcelo Siqueira Filho <marcelosiqqueira@gmail.com>
2023-03-02 19:57:12 -0300

    first commit

---
+++ src/routes/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import '../styles/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>ola</h1>
+    </div>
+  )
+}
+
+export default App
