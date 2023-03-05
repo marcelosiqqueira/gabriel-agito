8fa5bd8b0e759c7bd70c89a2cdb7218c633df593
Stablest <marlonlocci@hotmail.com>
2023-03-05 01:42:57 -0300

    added incomplete handleScrollElement

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,42 @@
-function Header(){
-    return(
+function Header() {
+
+    function handleScrollElement(e: any) {
+        // document.getElementById('')
+        switch (e.target.value) {
+            case 'inicio':
+                document.getElementById('')
+                break
+            case 'eventos':
+                document.getElementById('')
+                break
+            case 'calendario':
+                document.getElementById('')
+                break
+            case 'sobre':
+                document.getElementById('')
+                break
+        }
+    }
+
+    return (
         <header>
             <img src="" alt="logo" />
             <nav>
-            <button id='buttonHome'>
-                Inicio
-            </button>
+                <button value='inicio' id='buttonHome' onClick={e => handleScrollElement(e)}>
+                    Inicio
+                </button>
 
-            <button id='buttonEvents'>
-                Eventos
-            </button>
+                <button value='eventos' id='buttonEvents' onClick={e => handleScrollElement(e)}>
+                    Eventos
+                </button>
 
-            <button id='buttonCalendar'>
-                Calendário
-            </button>
+                <button value='calendario' id='buttonCalendar' onClick={e => handleScrollElement(e)}>
+                    Calendário
+                </button>
 
-            <button id='buttonAbout'>
-                Sobre
-            </button>
+                <button value='sobre' id='buttonAbout' onClick={e => handleScrollElement(e)}>
+                    Sobre
+                </button>
             </nav>
         </header>
     )
