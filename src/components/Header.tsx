7d9726b26d1232de227615a2c3e320bbf220a50b
Stablest <marlonlocci@hotmail.com>
2023-03-05 02:50:10 -0300

    completed scroll to element

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,23 @@
 function Header() {
+
+    function handleScrollElement(e: any) {
+        // document.getElementById('')
+        switch (e.target.value) {
+            case 'inicio':
+                document.getElementById('home')?.scrollIntoView(true)
+                break
+            case 'eventos':
+                document.getElementById('events')?.scrollIntoView(true)
+                break
+            case 'calendario':
+                document.getElementById('events')?.scrollIntoView(true)
+                break
+            case 'sobre':
+                document.getElementById('about')?.scrollIntoView(true)
+                break
+        }
+    }
+
     return (
         <header>
             <img src="" alt="logo" />
