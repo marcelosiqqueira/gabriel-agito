ca81a30e9ac9c61522a94bb6aefe6b26e2fd0068
Bianca Pena <120587014+Krepixie@users.noreply.github.com>
2023-03-11 20:23:44 -0300

    added location info for each list item and fixed image carousel buttons not working

--- src/components/UI/molecules/ListItem/ListItem.tsx
+++ src/components/UI/molecules/ListItem/ListItem.tsx
@@ -3,16 +3,23 @@ import './ListItem.css'
 export default function ListItem() {
     const star = 'src/assets/star.svg'
     const clock = 'src/assets/clock.svg'
+    const location = 'src/assets/location.svg'
     return (
         <li className="list-item">
             <div>
-                <img src={star} alt="star image" />
+                <img src={star} alt="star icon" />
                 <span>Orci varius natoque penatibus et magnis</span>
             </div>
 
             <div>
-                <img src={clock} alt="clock image" />
-                <span>11 fev 2020</span>
+                <div>
+                    <img src={clock} alt="clock icon" />
+                    <span>11 fev 2020</span>
+                </div>
+                <div>
+                    <img src={location} alt="location pin icon" />
+                    <span>Uberaba</span>
+                </div>
             </div>
         </li>
     )
--- src/components/UI/organisms/ImageCarousel.tsx
+++ src/components/UI/organisms/ImageCarousel.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 export default function ImageCarousel() {
-    const testImages = ['https://source.unsplash.com/random', 'https://source.unsplash.com/random', 'https://source.unsplash.com/random', 'https://source.unsplash.com/random', 'https://source.unsplash.com/random', 'https://source.unsplash.com/random']
+    const testImages = ['https://source.unsplash.com/random?sig=1', 'https://source.unsplash.com/random?sig=2', 'https://source.unsplash.com/random?sig=3', 'https://source.unsplash.com/random?sig=4', 'https://source.unsplash.com/random?sig=5', 'https://source.unsplash.com/random?sig=6']
     const [carouselIndex, setCarouselIndex] = useState(0)
 
     function handleButtonClick(e: any) {
@@ -15,8 +15,8 @@ export default function ImageCarousel() {
     return (
         <div id="image-carousel">
             <img src={testImages[carouselIndex]} alt="" />
-            <button value="left" className="button-carousel-left" onClick={e => handleButtonClick(e)}><span>Previous image</span></button>
-            <button value="right" className="button-carousel-right" onClick={e => handleButtonClick(e)}><span>Next image</span></button>
+            <button value="left" className="button-carousel-left" onClick={e => handleButtonClick(e)}>Previous image</button>
+            <button value="right" className="button-carousel-right" onClick={e => handleButtonClick(e)}>Next image</button>
         </div>
     )
 }
