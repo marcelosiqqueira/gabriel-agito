6087fbe8caba8b6a8fee8cfad01f9c365a0f3d32
Stablest <marlonlocci@hotmail.com>
2023-03-20 18:04:52 -0300

    changed api url

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,5 +1,5 @@
 export const basePhotoUrl: string =
   "https://drive.google.com/uc?export=view&id=";
 
-// export const apiUrl: string = "https://gabriel-agito-back.onrender.com/events/";
-export const apiUrl: string = "http://localhost:3333/events/";
+export const apiUrl: string = "https://gabriel-agito-back.onrender.com/events/";
+// export const apiUrl: string = "http://localhost:3333/events/";
