a5dc02953b1afffcfd0f5a11e7bcd2a6a572bcc6
Stablest <marlonlocci@hotmail.com>
2023-03-20 07:42:22 -0300

    added localhost const for testing

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,3 +1,5 @@
 export const basePhotoUrl: string =
   "https://drive.google.com/uc?export=view&id=";
-export const apiUrl: string = "https://gabriel-agito-back.onrender.com/events/";
+
+// export const apiUrl: string = "https://gabriel-agito-back.onrender.com/events/";
+export const apiUrl: string = "http://localhost:3333/events/";
