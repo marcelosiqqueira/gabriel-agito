c660166031891fd509a16e25096c8d86712366b5
Stablest <marlonlocci@hotmail.com>
2023-03-21 17:53:45 -0300

    added image carousel to component

--- src/components/UI/organisms/HomeMain/HomeMain.tsx
+++ src/components/UI/organisms/HomeMain/HomeMain.tsx
@@ -1,21 +1,22 @@
 import './HomeMain.css'
+import MainImageCarousel from '../MainImageCarousel/MainImageCarousel'
 
-export default function HomeMain(){
-    return(
-        <section id='home'>
-          <div className='home-description'>
-            <h2>Nome do evento</h2>
-            <div className='home-info'>
-              <span>Data: </span>
-              <span>??/??/??</span>
-              <br/>
-              <span>Local: </span>
-              <span>Local teste</span>
-            </div>        
-          </div>
-          <div className='home-images'>
-            <img src="placeholder-image.jpg"/>
-          </div>
-        </section>
-    )
+export default function HomeMain({ coverageEvents }: any) {
+  return (
+    <section id='home'>
+      <div className='home-description'>
+        <h2>Nome do evento</h2>
+        <div className='home-info'>
+          <span>Data: </span>
+          <span>??/??/??</span>
+          <br />
+          <span>Local: </span>
+          <span>Local teste</span>
+        </div>
+      </div>
+      <div className='home-images'>
+        <MainImageCarousel coverageEvents={coverageEvents}></MainImageCarousel>
+      </div>
+    </section>
+  )
 }
