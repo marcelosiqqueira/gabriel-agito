f92550c93dc2b5af621fb29bd424da21963d657f
Stablest <marlonlocci@hotmail.com>
2023-03-24 17:09:51 -0300

    removed useEffect

--- src/components/UI/organisms/ImageModal/ImageModal.tsx
+++ src/components/UI/organisms/ImageModal/ImageModal.tsx
@@ -1,16 +1,8 @@
 import './ImageModal.css'
-import { useEffect, useRef } from 'react'
+import { useRef } from 'react'
 
 export default function ImageModal({ carouselState: { carouselIndex, setCarouselIndex }, imageArray, onClose, setShowModal }: any) {
-
-    var focusableEls = document.body.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
-
-    const divRef = useRef<HTMLDivElement>(null)
-    useEffect(() => {
-        divRef.current?.focus()
-    }, [ImageModal])
-
-    console.log('imageModal renderizou')
+    const divRef = useRef<any>(null)
 
     function handleKeyboardInput(event: any) {
         if (event.key === 'ArrowLeft')
@@ -20,11 +12,10 @@ export default function ImageModal({ carouselState: { carouselIndex, setCarousel
         if (event.key === 'Escape')
             setShowModal(false)
         event.preventDefault()
-
     }
 
     return (
-        <div id='imageModal' ref={divRef} tabIndex={1} onKeyDown={handleKeyboardInput} aria-modal={true}>
+        <div id='imageModal' ref={divRef} tabIndex={1} onKeyDown={handleKeyboardInput} aria-modal={true} onLoad={() => divRef.current?.focus()}>
             <img src={imageArray[carouselIndex]?.src} alt="image" tabIndex={0} />
             <button onClick={onClose}><span>Close</span></button>
             <button value="left" className="button-carousel-left" onClick={() => (carouselIndex > 0) && setCarouselIndex(carouselIndex - 1)}><span>Previous image</span></button>
